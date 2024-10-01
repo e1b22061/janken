f7ebaa6add73c094917eacdfe47dc891ac27e01c
e1b22061 <e1b22061@oit.ac.jp>
2024-10-01 20:49:59 +0900

    じゃんけんゲームの実装

--- src/main/java/oit/is/z2620/kaizi/janken/controller/JankenController.java
+++ src/main/java/oit/is/z2620/kaizi/janken/controller/JankenController.java
@@ -3,11 +3,11 @@ package oit.is.z2620.kaizi.janken.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
-// import org.springframework.web.bind.annotation.GetMapping;
-// import org.springframework.web.bind.annotation.GetMapping;
 // import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import java.util.Map;
+import oit.is.z2620.kaizi.janken.model.Janken;
 
 @Controller
 public class JankenController {
@@ -18,7 +18,13 @@ public class JankenController {
   }
 
   @GetMapping("/janken")
-  public String janken() {
-    return "janken.html";
+  public String playJanken(@RequestParam String playerHand, ModelMap model) {
+    Janken janken = new Janken();
+    Map<String, String> outcome = janken.judge(playerHand);
+
+    model.addAttribute("playerHand", outcome.get("playerHand"));
+    model.addAttribute("cpuHand", outcome.get("cpuHand"));
+    model.addAttribute("result", outcome.get("result"));
+    return "janken.html"; // 結果を表示するページに遷移
   }
 }
