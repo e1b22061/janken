71a74637426664ced86d1b789d15af0bcb4074e6
e1b22061 <e1b22061@oit.ac.jp>
2024-10-08 18:31:49 +0900

    認証処理を実装

--- src/main/java/oit/is/z2620/kaizi/janken/model/Janken.java
+++ src/main/java/oit/is/z2620/kaizi/janken/model/Janken.java
@@ -7,19 +7,21 @@ public class Janken {
   public Map<String, String> judge(String playerHand) {
     String cpuHand = "r";
     String result = "";
-    if (playerHand.equals(cpuHand)) {
-      result = "引き分け";
-    } else if (playerHand.equals("s")) {
-      result = "負け";
-    } else if (playerHand.equals("p")) {
-      result = "勝ち";
-    } else {
-      result = "Error";
-    }
     Map<String, String> outcome = new HashMap<>();
-    outcome.put("playerHand", translateHand(playerHand));
-    outcome.put("cpuHand", translateHand(cpuHand));
-    outcome.put("result", result);
+    if (playerHand != null) {
+      if (playerHand.equals(cpuHand)) {
+        result = "引き分け";
+      } else if (playerHand.equals("s")) {
+        result = "負け";
+      } else if (playerHand.equals("p")) {
+        result = "勝ち";
+      } else {
+        result = "Error";
+      }
+      outcome.put("playerHand", translateHand(playerHand));
+      outcome.put("cpuHand", translateHand(cpuHand));
+      outcome.put("result", result);
+    }
     return outcome;
   }
 
